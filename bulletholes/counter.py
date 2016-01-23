e609235f0b73691c1ae5e8e206e9ba1d932c6272
Taylor Swift <kelvinsthirteen@gmail.com>
2016-01-23 15:57:41 -0500

    fix negative tagging

--- bulletholes/counter.py
+++ bulletholes/counter.py
@@ -5,7 +5,16 @@ from collections import Counter
 class TCounter(Counter):
     def __le__(self, other):
         for elem, count in self.items():
-            newcount = count - other[elem]
-            if (newcount > 0 and count > 0) or (newcount < 0 and count < 0):
+            ocount = other[elem]
+            if not count or not ((count > 0 and ocount >= count) or (count < 0 and ocount <= count)):
                 return False
         return True
+    
+    def concat(self, other):
+        result = Counter()
+        for elem, count in self.items():
+            result[elem] = count + other[elem]
+        for elem, count in other.items():
+            if elem not in self:
+                result[elem] = count
+        return result

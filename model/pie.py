056adb1714c20826eb373c141056af53ef4a1443
kelvin <kelvinsong10@gmail.com>
2016-02-10 23:43:09 -0500

    pie charts are a bit nicer now

--- model/pie.py
+++ model/pie.py
@@ -1,5 +1,6 @@
-from math import pi
-from itertools import chain
+from math import pi, atan2
+from itertools import chain, accumulate
+from bisect import bisect
 
 from model.olivia import Atomic_text, Block
 from edit.paperairplanes import interpret_rgba
@@ -12,6 +13,10 @@ def _print_attrs(name, attrs):
 
 class PieChart(object):
     def __init__(self, L):
+        if 'radius' in L[0][1]:
+            self._radius = int(L[0][1]['radius'])
+        else:
+            self._radius = 89
         self._chart = L
         self._title = next(E for tag, E in L[1] if tag[0] == 'module:pie:title')
         slices, self._labels = zip( * (( (int(tag[1]['prop']), interpret_rgba(tag[1]['color'])), E) for tag, E in L[1] if tag[0] == 'module:pie:slice'))
@@ -22,7 +27,7 @@ class PieChart(object):
         self._FLOW = [Atomic_text(text) for text in chain([self._title], self._labels)]
 
     def represent(self, serialize, indent):
-        lines = [(indent, '<module:pie>')]
+        lines = [(indent, _print_attrs( * self._chart[0]))]
         for tag, E in self._chart[1]:
             lines.append((indent + 1, _print_attrs( * tag)))
             lines.extend(serialize(E, indent + 2))
@@ -30,37 +35,62 @@ class PieChart(object):
         lines.append((indent, '</module:pie>'))
         return lines
 
-    def _print_pie(self, cr, x, y):
-        t = 0
-        for percent, arc, color in self._slices:
-            cr.move_to(x, y)
-            cr.arc(x, y, 89, t, t + arc)
-            cr.close_path()
-            cr.set_source_rgba( * color)
-            cr.fill()
-            t += arc
-    
     def fill(self, bounds, c, y):
+        r = self._radius
         top = y
         # title
         self._FLOW[0].cast(bounds, c, y)
-        y += 20
+        y = self._FLOW[0]._SLUGS[-1]['y'] + 22
+        left, right = bounds.bounds(y + r)
+        px = (right + left)/2
+        py = y + r
         for S in self._FLOW[1:]:
             S.cast(bounds, c, y)
             y += 20
-        bottom = y
-        left, right = bounds.bounds(top)
-        return _MBlock(self._FLOW, top, bottom, left, right, lambda cr: self._print_pie(cr, (right + left)/2, top + 120))
+        bottom = py + r + 22
+        
+        return _MBlock(self._FLOW, top, bottom, left, right, self._slices, (px, py, self._radius))
 
 class _MBlock(Block):
-    def __init__(self, FLOW, top, bottom, left, right, paint):
+    def __init__(self, FLOW, top, bottom, left, right, slices, pie):
         Block.__init__(self, FLOW, top, bottom, left, right)
-        self._print = paint
+        self._slices = slices
+        self._slices_t = list(accumulate(s[1] for s in slices))
+        print(self._slices_t)
+        self._pie = pie
+        self._active = None
+    
+    def _print_pie(self, cr):
+        x, y, r = self._pie
+        t = 0
+        for i, S in enumerate(self._slices):
+            percent, arc, color = S
+            cr.move_to(x, y)
+            cr.arc(x, y, r, t, t + arc)
+            cr.close_path()
+            cr.set_source_rgba( * color)
+            cr.fill()
+            
+            if i == self._active:
+                cr.set_line_width(2)
+                cr.arc(x, y, r + 13, t, t + arc)
+                cr.stroke()
+            
+            t += arc
+    
+    def _target_slice(self, x, y):
+        px, py, r = self._pie
+        t = atan2(y - py, x - px)
+        if t < 0:
+            t += 2*pi
+        return bisect(self._slices_t, t)
     
     def I(self, x, y):
-        return self['j']
+        s = self._target_slice(x, y)
+        self._active = s
+        return self._FLOW[s + 1]
     
     def deposit(self, repository):
         for A in self._FLOW:
             A.deposit(repository)
-        repository['_paint'].append(self._print)
+        repository['_paint'].append(self._print_pie)

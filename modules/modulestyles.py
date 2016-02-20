454bd0fdc359ddbdb34dfa70895601049b3089d8
Taylor Swift <kelvinsthirteen@gmail.com>
2016-02-20 18:17:14 -0500

    make implicit styles persistent across undo/redo

--- modules/modulestyles.py
+++ modules/modulestyles.py
@@ -8,12 +8,16 @@ S= {'mod:bounded': {'symbol': {'big': 1},
 
 class MS_Library(dict):
     def __init__(self, modules):
+        # save modules list
+        self._modules = modules
+        self.turnover()
+    
+    def turnover(self):
         D = {}
-        for module in modules:
+        for module in self._modules:
             ns = module.namespace
             E = module.DNA.copy()
             if ns in S:
                 E.update(S[ns])
             D[module] = {T: Counter({styles.FTAGS[N]: C for N, C in V.items()}) for T, V in E.items()}
         dict.__init__(self, D)
-        

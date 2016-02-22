2ae9be53380a723e442884e6672981223a8e8022
Taylor Swift <kelvinsthirteen@gmail.com>
2016-02-21 21:32:33 -0500

    much better typographical space support: all types of spaces are visible as annotations and can be typed through xcompose addons

--- style/fonts.py
+++ style/fonts.py
@@ -15,15 +15,69 @@ class Memo_font(freetype.Face):
     
     def __init__(self, path):
         freetype.Face.__init__(self, path)
-        
-        self._widths = {
-                None: 0
-                }
-        
+        UPM = self.units_per_EM
         self._ordinals = {
+                '\u00A0': -30, # nbsp
+                '\u2003': -31, # em
+                '\u2002': -32, # en
+                '\u2004': -33, # 1/3
+                '\u2005': -34, # 1/4
+                '\u2006': -35, # 1/6
+                '\u2007': -36, # figure
+                '\u2008': -37, # punctuation
+                '\u2009': -38, # thin
+                '\u200A': -39, # hair
+                '\u202F': -40, # narrow nbsp
+                '\u205F': -41, # math med
+                
                 }
+                # '<p>':        -2
+                # '</p>':       -3
+                # '<f>':        -4
+                # '</f>':       -5
+                # '<br/>':      -6
+                # '<image/>':   -13
+                # '<mod:/>':    -89
+                
+                # 'ERROR':      -23
+        spacemap = {o: c for c, o in self._ordinals.items()}
+        space = self.get_advance(self.character_index(' '), True)/UPM
+        fig = self.get_advance(self.character_index('0'), True)/UPM
+        punc = self.get_advance(self.character_index(','), True)/UPM
+        self._widths = {
+                None: 0,
+                ' ': space,
+                '\u00A0': space,
+                '\u2003': 1, # em
+                '\u2002': 0.5, # en
+                '\u2004': 1/3, # 1/3
+                '\u2005': 0.25, # 1/4
+                '\u2006': 1/6, # 1/6
+                '\u2007': fig, # figure
+                '\u2008': punc, # punctuation
+                '\u2009': 0.2, # thin
+                '\u200A': 0.1, # hair
+                '\u202F': 0.2, # narrow nbsp
+                '\u205F': 4/18, # math med
+                }
+
+        self.spacenames = {
+                -30: 'nb', # nbsp
+                -31: 'em', # em
+                -32: 'en', # en
+                -33: '1/3', # 1/3
+                -34: '1/4', # 1/4
+                -35: '1/6', # 1/6
+                -36: 'fig', # figure
+                -37: 'pn', # punctuation
+                -38: '1/5', # thin
+                -39: 'h', # hair
+                -40: 'nnb', # narrow nbsp
+                -41: 'mt', # math med
+                }
+        self.spacewidths = {c: self._widths[spacemap[c]] for c in range(-41, -29)}
         
-        self.vmetrics = self.ascender / self.units_per_EM, self.descender / self.units_per_EM
+        self.vmetrics = self.ascender / UPM, self.descender / UPM
     
     def advance_pixel_width(self, character):
         try:

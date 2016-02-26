3bd1420d11d4db2ac4009994d42c26d42ba7c0c7
Taylor Swift <kelvinsthirteen@gmail.com>
2016-02-26 18:27:23 -0500

    unify graph blocks

--- modules/histogram.py
+++ modules/histogram.py
@@ -1,13 +1,13 @@
 from itertools import chain, accumulate
 from bisect import bisect
 
-from model.olivia import Atomic_text, Block
+from model.olivia import Atomic_text
 from model.cat import cast_mono_line
 from model.george import Subcell
 from elements.elements import Block_element
 from interface.base import accent
 
-from modules._graph import generate_key
+from modules._graph import generate_key, GraphBlock
 
 _namespace = 'mod:hist'
 
@@ -138,35 +138,7 @@ class Histogram(Block_element):
         
         self._ki, self._ky = generate_key(self._FLOW[2:], Subcell(bounds, 0.2, 1), c, top, py, P_key)
         
-        return _MBlock(self._FLOW, self._printed_numbers, (top, self._FLOW[0].y, left, right), self.draw_bars, self.bar_annot, (px, py), self.regions, self.PP)
-
-class _MBlock(Block):
-    def __init__(self, FLOW, MONO, box, draw, draw_annot, origin, regions, PP):
-        Block.__init__(self, FLOW, * box, PP)
-        self._origin = origin
-        self._MONO = MONO
-        self._draw = draw
-        self._draw_annot = draw_annot
-        self._regions = regions
-
-    def _print_annot(self, cr, O):
-        if O in self._FLOW:
-            self._draw_annot(cr)
-            self._handle(cr)
-            cr.fill()
-    
-    def I(self, x, y):
-        if x <= self['right']:
-            dx, dy = self._origin
-            s = min(len(self._FLOW) - 1, self._regions(x - dx, y - dy))
-            return self._FLOW[s]
-        else:
-            return self['i']
-    
-    def deposit(self, repository):
-        repository['_paint'].append((self._draw, * self._origin )) # come before to avoid occluding child elements
-        repository['_paint_annot'].append((self._print_annot, 0, 0))
-        for A in self._FLOW:
-            A.deposit(repository)
-        for A in self._MONO:
-            A.deposit(repository, * self._origin)
+        return GraphBlock(self._FLOW, self._printed_numbers, 
+                    (top, self._FLOW[0].y, left, right), 
+                    self.draw_bars, self.bar_annot, 
+                    (px, py), self.regions, self.PP)
--- modules/pie.py
+++ modules/pie.py
@@ -2,11 +2,11 @@ from math import pi, atan2
 from itertools import chain, accumulate
 from bisect import bisect
 
-from model.olivia import Atomic_text, Block
+from model.olivia import Atomic_text
 from model.george import Subcell
 from elements.elements import Block_element
 
-from modules._graph import generate_key
+from modules._graph import generate_key, GraphBlock
 
 _namespace = 'mod:pie'
 
@@ -85,31 +85,7 @@ class PieChart(Block_element):
         self._center = px, py
         self._to_right = right - px
         
-        return _MBlock(self._FLOW, (top, bottom, left, right), self.print_pie, self.pie_annot, self._center, self.regions, self.PP)
-
-class _MBlock(Block):
-    def __init__(self, FLOW, box, draw, draw_annot, origin, regions, PP):
-        Block.__init__(self, FLOW, * box, PP)
-        self._origin = origin
-        self._draw = draw
-        self._draw_annot = draw_annot
-        self._regions = regions
-
-    def _print_annot(self, cr, O):
-        if O in self._FLOW:
-            self._draw_annot(cr)
-            self._handle(cr)
-            cr.fill()
-    
-    def I(self, x, y):
-        if x <= self['right']:
-            px, py = self._origin
-            return self._FLOW[self._regions(x - px, y - py)]
-        else:
-            return self['i']
-    
-    def deposit(self, repository):
-        repository['_paint'].append((self._draw, * self._origin)) # come before to avoid occluding child elements
-        repository['_paint_annot'].append((self._print_annot, 0, 0))
-        for A in self._FLOW:
-            A.deposit(repository)
+        return GraphBlock(self._FLOW, [],
+                    (top, bottom, left, right), 
+                    self.print_pie, self.pie_annot, 
+                    self._center, self.regions, self.PP)

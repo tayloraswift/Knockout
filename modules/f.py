b5afad3343971c77a6c915230be15ca125637073
kelvin <kelvinsong10@gmail.com>
2016-03-02 21:13:04 -0500

    merge common graphing functions, improve graph handling of bad values, correct minus sign graph typesetting, log plots

--- modules/f.py
+++ modules/f.py
@@ -1,5 +1,5 @@
-from math import pi, ceil
-from modules._graph import Cartesian
+from math import ceil
+from modules._graph import Cartesian, cart_ADNA
 
 _namespace = 'mod:f'
 
@@ -11,57 +11,37 @@ class Function(Cartesian):
     namespace = _namespace
     tags = {_namespace + ':' + T for T in ('x', 'y', 'dataset')}
     
-    ADNA = {_namespace: [('height', 89, 'int'), ('tickwidth', 0.5, 'float'), ('round', 13, 'int')],
-            'x': [('start', 0, 'float'), ('step', 1, 'float'), ('minor', 1, 'int'), ('major', 2, 'int'), ('every', 2, 'int'), ('stop', 89, 'float')],
-            'y': [('start', 0, 'float'), ('step', 22.25, 'float'), ('minor', 1, 'int'), ('major', 2, 'int'), ('every', '2', 'int'), ('stop', 89, 'float')],
+    ADNA = {_namespace: cart_ADNA['GRAPH'],
+            'x': cart_ADNA['x'],
+            'y': cart_ADNA['y'],
             'dataset': [('f', lambda x: x, 'fx'), ('color', '#ff3085', 'rgba')]}
     documentation = [(0, _namespace), (1, 'x'), (1, 'y'), (1, 'dataset')]
     
     def process_data(self, functions, x0, dx, xx, y0, dy, yy):
         P = []
         for F in functions:
-            g = []
+            g = [[]]
             for x in f_numeric(x0, x0 + xx, dx):
                 try:
                     y = F(x)
-                except ZeroDivisionError:
-                    y = 0
-                g.append((self.U(x - x0)/xx, self._graphheight*self.V(y - y0)/yy))
+                    g[-1].append(((self.U(x) - x0)/xx, self._graphheight*(self.V(y) - y0)/yy))
+                    continue
+                except (ZeroDivisionError, ValueError):
+                    pass
+                if g[-1]:
+                    g.append([])
+                
             P.append(g)
-        self._points = P
-        
-    def ink_graph(self, cr):
-        # ticks
-        # x
-        tw = self._tw
-        cr.set_source_rgb(0, 0, 0)
-        for i, x in enumerate(self._origins):
-            if self._xnumbers[i][2]:
-                cr.rectangle(x, 0, tw, 8)
-            elif self._xnumbers[i][1]:
-                cr.rectangle(x, 0, tw, 4)
-        # y
-        for y, m, M, s, k in self._ynumbers:
-            if M:
-                cr.rectangle(0, -int(round(y)), -8, -tw)
-            elif m:
-                cr.rectangle(0, -int(round(y)), -4, -tw)
-        cr.fill()
-        
-        # points
-        end = self._origins[-1]
-        circle_t = 2*pi
+        return P
+    
+    def _draw_data(self, cr, data):
         cr.set_line_width(2)
-        for pointset, color, (k1, k2) in zip(self._t_points, self._datacolors, self._ky):
-            cr.set_source_rgba( * color )
-            cr.move_to( * pointset[0] )
-            for x, y in pointset[1:]:
+        for segment in data:
+            cr.move_to( * segment[0] )
+            for x, y in segment[1:]:
                 cr.line_to(x, y)
-            cr.stroke()
-            # key
-            cr.rectangle(end, k1 + 4, -4, k2 - k1 - 4)
-            cr.fill()
+        cr.stroke()
     
     def transform_data(self, width):
         # TRANSFORM POINTS
-        self._t_points = [[(x*width, -y) for x, y in pointset] for pointset in self._points]
+        return list(zip([[[(x*width, -y) for x, y in segment] for segment in pointset] for pointset in self._data_unscaled], self._datacolors, self._ky))
--- modules/scatterplot.py
+++ modules/scatterplot.py
@@ -1,6 +1,5 @@
 from math import pi
-
-from modules._graph import Cartesian
+from modules._graph import Cartesian, cart_ADNA
 
 _namespace = 'mod:scatter'
 
@@ -8,46 +7,22 @@ class Scatterplot(Cartesian):
     namespace = _namespace
     tags = {_namespace + ':' + T for T in ('x', 'y', 'dataset')}
     
-    ADNA = {_namespace: [('height', 89, 'int'), ('tickwidth', 0.5, 'float'), ('round', 13, 'int')],
-            'x': [('start', 0, 'float'), ('step', 1, 'float'), ('minor', 1, 'int'), ('major', 2, 'int'), ('every', 2, 'int'), ('stop', 89, 'float')],
-            'y': [('start', 0, 'float'), ('step', 22.25, 'float'), ('minor', 1, 'int'), ('major', 2, 'int'), ('every', '2', 'int'), ('stop', 89, 'float')],
+    ADNA = {_namespace: cart_ADNA['GRAPH'],
+            'x': cart_ADNA['x'],
+            'y': cart_ADNA['y'],
             'dataset': [('data', (), '2D'), ('color', '#ff3085', 'rgba')]}
     documentation = [(0, _namespace), (1, 'x'), (1, 'y'), (1, 'dataset')]
     
     def process_data(self, datavalues, x0, dx, xx, y0, dy, yy):
-        self._points = [[(self.U(x - x0)/xx, self._graphheight*self.V(y - y0)/yy) for x, y in VV] for VV in datavalues]
-        
-    def ink_graph(self, cr):
-        # ticks
-        # x
-        tw = self._tw
-        cr.set_source_rgb(0, 0, 0)
-        for i, x in enumerate(self._origins):
-            if self._xnumbers[i][2]:
-                cr.rectangle(x, 0, tw, 8)
-            elif self._xnumbers[i][1]:
-                cr.rectangle(x, 0, tw, 4)
-        # y
-        for y, m, M, s, k in self._ynumbers:
-            if M:
-                cr.rectangle(0, -int(round(y)), -8, -tw)
-            elif m:
-                cr.rectangle(0, -int(round(y)), -4, -tw)
-        cr.fill()
-        
-        # points
-        end = self._origins[-1]
+        return [[((self.U(x) - x0)/xx, self._graphheight*(self.V(y) - y0)/yy) for x, y in VV] for VV in datavalues]
+    
+    def _draw_data(self, cr, data):
         circle_t = 2*pi
-        for pointset, color, (k1, k2) in zip(self._t_points, self._datacolors, self._ky):
-            cr.set_source_rgba( * color )
-            for x, y in pointset:
-                cr.move_to(x, y)
-                cr.arc(x, y, 2, 0, circle_t)
-                cr.close_path()
-            # key
-            cr.rectangle(end, k1 + 4, -4, k2 - k1 - 4)
-            cr.fill()
+        for x, y in data:
+            cr.move_to(x, y)
+            cr.arc(x, y, 2, 0, circle_t)
+            cr.close_path()
     
     def transform_data(self, width):
         # TRANSFORM POINTS
-        self._t_points = [[(x*width, -y) for x, y in pointset] for pointset in self._points]
+        return list(zip([[(x*width, -y) for x, y in pointset] for pointset in self._data_unscaled], self._datacolors, self._ky))

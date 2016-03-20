95f7095490d3724a51d82739c3a2da396db20613
Taylor Swift <kelvinsthirteen@gmail.com>
2016-03-20 14:56:15 -0400

    bubble plots

--- modules/plot/scatterplot.py
+++ modules/plot/scatterplot.py
@@ -1,4 +1,4 @@
-from math import pi
+from math import pi, sqrt
 from .data import Data
 
 class Scatterplot(Data):
@@ -25,4 +25,28 @@ class Scatterplot(Data):
         project = axes.project
         self._points = [project( * coords ) for coords in self._unitpoints]
 
-members = [Scatterplot]
+class Bubbleplot(Data):
+    name = 'mod:bubble'
+    ADNA = [('data', (), 'multi_D'), ('color', '#ff3085', 'rgba'), ('radius', lambda a: sqrt(abs(a)), 'fx'), ('key', True, 'bool')]
+
+    def unit(self, axes):
+        fit = axes.fit
+        size = self['radius']
+        self._unitpoints = [(fit( * coord[:-1] ), size(coord[-1])) if len(coord) > 2 else (fit( * coord ), size(-1)) for coord in self['data']]
+        return self
+    
+    def draw(self, cr):
+        cr.set_source_rgba( * self['color'] )
+        circle_t = 2*pi
+        for (x, y), size in self._points:
+            cr.move_to(x, y)
+            cr.arc(x, y, size, 0, circle_t)
+            cr.close_path()
+            cr.fill()
+    
+    def freeze(self, axes):
+        self._right = axes.h
+        project = axes.project
+        self._points = [(project( * coords ), size) for coords, size in self._unitpoints]
+
+members = [Scatterplot, Bubbleplot]

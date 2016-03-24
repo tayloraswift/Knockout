35af7e34e8611ea255d6b9eeb60adc6af93916fa
Taylor Swift <kelvinsthirteen@gmail.com>
2016-03-24 17:58:58 -0400

    slight refactor with implicit module styles

--- modules/modulestyles.py
+++ modules/modulestyles.py
@@ -1,7 +1,11 @@
 from bulletholes.counter import TCounter as Counter
 from style import styles
 
-S= {'mi': {'mi': {('f', 'emphasis'): 1}},
+S= {'_global': {'_left': {('p', '_left'): 1},
+                '_center': {('p', '_center'): 1},
+                '_right': {('p', '_right'): 1}},
+    
+    'mi': {'mi': {('f', 'emphasis'): 1}},
     'mod:bounded': {'symbol': {('f', 'big'): 1},
                 'bounds': {('f', 'small'): 1}},
     'mod:root': {'index': {('f', 'small'): 1}},
@@ -12,9 +16,8 @@ S= {'mi': {'mi': {('f', 'emphasis'): 1}},
     'mod:pie': {'slice': {('p', 'key'): 1}},
     
     'mod:plot': {'num': {('f', 'small'): 1},
-                'dataset': {('p', 'key'): 1},
-                'y': {('p', 'tablecell'): 1, ('p', 'tablehead'): 1},
-                'x': {('p', 'tablecell'): 1, ('p', 'tablehead'): 1}}
+                'key': {('p', 'key'): 1},
+                'axis': {('p', 'tablecell'): 1, ('p', 'tablehead'): 1}}
         }
 
 class MS_Library(dict):
@@ -28,6 +31,7 @@ class MS_Library(dict):
         TAGS = {'f': styles.FTAGS, 'p': styles.PTAGS}
         for ns, module in self._modslist.items():
             E = module.DNA.copy()
+            E.update(S['_global'])
             if ns in S:
                 E.update(S[ns])
             D[module] = {T: Counter(dict((TAGS[level][N], C) for (level, N), C in V.items())) for T, V in E.items()}
